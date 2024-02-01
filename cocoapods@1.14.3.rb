9197b787b836468c3fc5d5b2911c75d8f088609a
kkaczynski <69236873+kkaczynski@users.noreply.github.com>
2024-02-01 17:27:18 +0100

    comment bottle

--- cocoapods@1.14.3.rb
+++ cocoapods@1.14.3.rb
@@ -5,15 +5,15 @@ class CocoapodsAT1143 < Formula
   sha256 "de05766e5771e0cef7af89f73b0e42a1f1c52a76ce1288592cd9511bcd688a9e"
   license "MIT"
 
-  bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "659ed73f0da29fb6bc21269a51d83d60ac069cc0ca42548776149a2a1f839928"
-    sha256 cellar: :any,                 arm64_ventura:  "b2b02391270c4397ed07fc3f547a0956562e9c916d6c901931e4b6d9b7f59416"
-    sha256 cellar: :any,                 arm64_monterey: "c4da45ab76b118bf8336704fd134209341efcbfbd0d6f9ebb439ded268802a35"
-    sha256 cellar: :any,                 sonoma:         "4bcebefe7753f3db1be8379844e0eb987c530ee5069027a574f096ee3a7ed1c9"
-    sha256 cellar: :any,                 ventura:        "139bf64804df7abcf4465e6e71e03bf8d5cc9d8b39306c0b001fe7de16f3f349"
-    sha256 cellar: :any,                 monterey:       "d27fccc4a9f4ecb8b8384ecd764878137c5ec0742eb4080bf87f409a3007e66b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "97254aa1331bd0dcf0e1d22d9166c56767a03418586bf213d5b19555c4a6cfd4"
-  end
+  #bottle do
+  #  sha256 cellar: :any,                 arm64_sonoma:   "659ed73f0da29fb6bc21269a51d83d60ac069cc0ca42548776149a2a1f839928"
+  #  sha256 cellar: :any,                 arm64_ventura:  "b2b02391270c4397ed07fc3f547a0956562e9c916d6c901931e4b6d9b7f59416"
+  #  sha256 cellar: :any,                 arm64_monterey: "c4da45ab76b118bf8336704fd134209341efcbfbd0d6f9ebb439ded268802a35"
+  #  sha256 cellar: :any,                 sonoma:         "4bcebefe7753f3db1be8379844e0eb987c530ee5069027a574f096ee3a7ed1c9"
+  #  sha256 cellar: :any,                 ventura:        "139bf64804df7abcf4465e6e71e03bf8d5cc9d8b39306c0b001fe7de16f3f349"
+  #  sha256 cellar: :any,                 monterey:       "d27fccc4a9f4ecb8b8384ecd764878137c5ec0742eb4080bf87f409a3007e66b"
+  #  sha256 cellar: :any_skip_relocation, x86_64_linux:   "97254aa1331bd0dcf0e1d22d9166c56767a03418586bf213d5b19555c4a6cfd4"
+  #end
 
   depends_on "pkg-config" => :build
   depends_on "ruby"
