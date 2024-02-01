79f264b645b928ad1d913f8333f9b53aba3c50e0
kkaczynski <69236873+kkaczynski@users.noreply.github.com>
2024-02-01 17:19:54 +0100

    Change name of cocoapods

--- cocoapods@1.14.3.rb
+++ cocoapods@1.14.3.rb
@@ -1,4 +1,4 @@
-class Cocoapods < Formula
+class CocoapodsAT1143 < Formula
   desc "Dependency manager for Cocoa projects"
   homepage "https://cocoapods.org/"
   url "https://github.com/CocoaPods/CocoaPods/archive/refs/tags/1.14.3.tar.gz"
